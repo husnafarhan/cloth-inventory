fadff287803ccb64308734946e1a2691cf2d09d0
husnafarhan <ajmalhusna04@gmail.com>
2026-06-11 14:06:51 +0530

    Added sales module

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Sale
 
+admin.site.register(Sale)
 # Register your models here.
--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class Sale(models.Model):
+    customer_name = models.CharField(max_length=100)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    sale_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer_name
