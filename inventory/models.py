411478e227ab6a63d5aec940ea9047575655d94d
husnafarhan <ajmalhusna04@gmail.com>
2026-06-11 14:03:53 +0530

    Added inventory module

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class StockTransaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('IN', 'Stock In'),
+        ('OUT', 'Stock Out'),
+    )
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=3, choices=TRANSACTION_TYPES)
+    quantity = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.transaction_type}"
