2ad1611a3b07fec012fd25922ef900f9d4397402
husnafarhan <ajmalhusna04@gmail.com>
2026-06-11 14:01:01 +0530

    Added product models and admin registration

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+from .models import Category, Brand, Supplier, Product
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Supplier)
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    product_code = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=200)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+
+    size = models.CharField(max_length=20)
+    color = models.CharField(max_length=30)
+
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
